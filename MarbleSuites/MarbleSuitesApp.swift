d6bf616b632cc1d5dc33c7c83d14ec69310ea1ab
Jcoub <joshuagyacoub@gmail.com>
2022-02-02 21:08:25 -0500

    Initial Commit

---
+++ MarbleSuites/MarbleSuitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarbleSuitesApp.swift
+//  MarbleSuites
+//
+//  Created by Owner on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct MarbleSuitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
